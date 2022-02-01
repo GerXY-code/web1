14c4e4f83b60364a9df181a4a2b773621cf93bc2
Lehoczki Gergő <gerixy20@gmail.com>
2022-02-01 22:53:48 +0100

    projekt hozzáadva

---
+++ web1/gallery.js
@@ -0,0 +1,32 @@
+
+function wallpapers(){
+    document.getElementById("first").style.display='block';
+    document.getElementById("second").style.display='none';
+}
+
+
+function animals(){
+    document.getElementById("first").style.display='none';
+    document.getElementById("second").style.display='block';
+}
+function allp(){
+    document.getElementById("first").style.display='block';
+    document.getElementById("second").style.display='block';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ web1/videocollection.js
@@ -0,0 +1,12 @@
+function showpOne() {
+    document.getElementById("show").style.display='block';
+    document.getElementById("hider").style.display='none';
+    document.getElementById("pageActual").innerHTML = "1";
+    
+  }
+  function showpTwo() {
+    document.getElementById("show").style.display='none';
+    document.getElementById("hider").style.display='block';
+    document.getElementById("pageActual").innerHTML = "2";
+    
+  }
